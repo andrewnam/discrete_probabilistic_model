b6e3ef34cbc801454885cd32b226059d58ccae8b
Andrew Nam <andrewnam@berkeley.edu>
2022-12-20 12:03:37 -0800

    Added requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ 
+    'numpy',
+    'frozendict'
+]
 
 test_requirements = [ ]
 
@@ -39,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/andrewnam/discrete_probabilistic_model',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
